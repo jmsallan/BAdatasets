436d179d97c6373c4d211c50ddeb72063f2a0f07
jmsallan <jose.maria.sallan@gmail.com>
2020-04-16 08:56:55 +0200

    renaming phase 2

--- R/datasets.R
+++ R/datasets.R
@@ -1,3 +1,31 @@
+#' Wine quality data set
+#'
+#' @description Two datasets with chemical properties of red and white vinho verde wine samples, from the north of Portugal.
+#'
+#' @format A list with two data frames for red and white wine.
+#'
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/wine+quality}
+#'
+#' @examples
+#'
+#' WineQuality$red
+#'
+"WineQuality"
+
+#' Data for insurance premium prediction
+#'
+#' @description A dataset to predict the insurance premium (charges).
+#'
+#' @format A data frame with 1338 observations (rows) and 7 features (columns). The dataset contains four numerical features (age, bmi, children and expenses) and three nominal features (sex, smoker and region) converted into factors with numerical value designated for each level. The variable to predict is charges.
+#'
+#' @source \url{https://www.kaggle.com/noordeen/insurance-premium-prediction}. Original source was  from the Machine Learning course website (Spring 2017) from Professor Eric Suess.
+#'
+#' @examples
+#'
+#' InsuranceCharges
+#'
+"InsuranceCharges"
+
 #' default of credit card clients
 #'
 #' @description A dataset of default payments in Taiwan, used in Yeh, I. C., & Lien, C. H. (2009). The comparisons of data mining techniques for the predictive accuracy of probability of default of credit card clients. Expert Systems with Applications, 36(2), 2473-2480.
