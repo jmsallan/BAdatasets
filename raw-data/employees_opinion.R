436575c0358fc49af2ee60d921ebf3952a4b2dc2
VicencFernandez <vicenc.fernandez@gmail.com>
2021-05-11 10:55:13 +0200

    new_dataset_employee_opinion

---
+++ raw-data/employees_opinion.R
@@ -0,0 +1,12 @@
+#---- Employee Opinion -----
+
+library(tidyverse)
+
+#set working directory to raw-data
+employees_opinion <- read.csv("employees_opinion.csv", sep = ";") %>% as_tibble()
+employees_opinion$assessment <- as.factor(employees_opinion$assessment)
+
+#set working directory to data
+save(employees_opinion, file="employees_opinion.RData")
+
+devtools::use_data(employees_opinion, overwrite = TRUE)
